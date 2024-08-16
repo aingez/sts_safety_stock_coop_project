bba531c0f0113d15ab50bb905fece8b575a94ea9
aingez <aingthawan@gmail.com>
2024-08-16 22:41:21 +0700

    nav ui

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 function Nav() {
   return (
-    <ul className='text-3xl mb-10 flex justify-left p-3 bg-black'>
+    <ul className='text-[20px] mb-10 flex justify-left p-3 bg-black'>
         <li className='mx-2 font-bold text-white transition-all hover:text-gray-400'><Link href="/">M-Stock</Link></li>
         <li className='mx-2 text-white transition-all hover:text-gray-400'><Link href="/pack">Pack</Link></li>
         <li className='mx-2 text-white transition-all hover:text-gray-400'><Link href="/unpack">Unpack</Link></li>
