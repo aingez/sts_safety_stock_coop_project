a6a4be7c20d7158f2b568b6196f25e1441fbd005
aingez <aingthawan@gmail.com>
2024-08-26 16:22:34 +0700

    chart display update

--- src/app/components/donutDash.jsx
+++ src/app/components/donutDash.jsx
@@ -5,49 +5,23 @@ import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 ChartJS.register(ArcElement, Tooltip, Legend, ChartDataLabels);
 
-function DoughnutChart({ data, chartName}) {
-  // Check data[0].model is valid
-  const parsedData = data.length > 0 && Array.isArray(data[0].model)
-    ? data[0].model.map(model => ({
-        label: model.model,
-        qty: model.qty,
-      }))
-    : [];
-
-  // Summing up the parts
-  const sumParts = parsedData.reduce((sum, item) => sum + item.qty, 0);
-
+function DoughnutChart({ data, chartName }) {
+  // Parse the data to extract labels and values
   const chartData = {
-    labels: parsedData.map(item => item.label),
+    labels: data.map(item => item.type),
     datasets: [
       {
-        data: parsedData.map(item => item.qty),
-        // get color scheme from https://www.learnui.design/tools/data-color-picker.html#single
-        backgroundColor: [
-          "#1f77b4", // Strong blue
-          "#ff7f0e", // Bright orange
-          "#2ca02c", // Vivid green
-          "#d62728", // Bold red
-          "#9467bd", // Deep purple
-          "#8c564b", // Warm brown
-        ],
-        hoverBackgroundColor: [
-          "#145a7a", // Darker blue
-          "#bf5a00", // Darker orange
-          "#1d7a1d", // Darker green
-          "#a31d1d", // Darker red
-          "#674a89", // Darker purple
-          "#643d35", // Darker brown
-        ],
-
+        data: data.map(item => item.total),
+        backgroundColor: ["#a60a0a", "#bc432e", "#d16a53", "#e38e79", "#f2b2a1", "#ffd7cc"],
+        hoverBackgroundColor: ["#a60a0a", "#b71e15", "#c92e1f", "#db3c2a", "#ed4934", "#ff563f"],
       },
     ],
   };
 
+  // Options for the Doughnut chart
   const defaultOptionsDoughnut = {
     responsive: true,
-    maibtainAspectRatio: false,
-    // aspectRatio: 0.5,
+    maintainAspectRatio: false,
     plugins: {
       legend: {
         display: false,
@@ -61,40 +35,49 @@ function DoughnutChart({ data, chartName}) {
         },
         formatter: (value, ctx) => {
           let label = ctx.chart.data.labels[ctx.dataIndex];
-          return label;
+          return `${label}: ${value}`;
+        },
+      },
+      tooltip: {
+        callbacks: {
+          label: (tooltipItem) => {
+            const label = tooltipItem.label;
+            const value = tooltipItem.raw;
+            return `${label}: ${value}`;
+          },
         },
       },
     },
     centerText: {
       display: true,
-      text: `${chartName}: ${sumParts}`,
+      text: `${chartName}`,
     },
   };
 
-  // Add this new plugin to draw the center text
-  const textCenter = {
-    id: 'textCenter',
-    afterDraw: (chart) => {
-      if (chart.config.options.centerText.display !== false) {
-        const ctx = chart.ctx;
-        const centerConfig = chart.config.options.centerText;
+  // Custom plugin for center text
+  // const textCenter = {
+  //   id: 'textCenter',
+  //   afterDraw: (chart) => {
+  //     if (chart.config.options.centerText.display !== false) {
+  //       const ctx = chart.ctx;
+  //       const centerConfig = chart.config.options.centerText;
 
-        const chartArea = chart.chartArea;
-        const textX = (chartArea.left + chartArea.right) / 2;
-        const textY = (chartArea.top + chartArea.bottom) / 2;
+  //       const chartArea = chart.chartArea;
+  //       const textX = (chartArea.left + chartArea.right) / 2;
+  //       const textY = (chartArea.top + chartArea.bottom) / 2;
 
-        ctx.font = "bold 13px Arial";
-        ctx.fillStyle = "black";
-        ctx.textAlign = 'center';
-        ctx.textBaseline = 'middle';
+  //       ctx.font = "bold 13px Arial";
+  //       ctx.fillStyle = "black";
+  //       ctx.textAlign = 'center';
+  //       ctx.textBaseline = 'middle';
 
-        ctx.fillText(centerConfig.text, textX, textY);
-      }
-    }
-  };
+  //       ctx.fillText(centerConfig.text, textX, textY);
+  //     }
+  //   }
+  // };
 
   // Register the plugin
-  ChartJS.register(textCenter);
+  // ChartJS.register(textCenter);
 
   return (
     <div style={{ width: 300, height: 300 }}>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,60 +1,15 @@
 'use client'
 
+import React, { useState, useEffect } from 'react';
 import DoughnutChart from './components/donutDash';
 import BarChart from './components/barDash';
 import ReusableTable from './components/alertTable';  // Import the reusable table
 import WarehouseDash from './components/warehouseDash'; // Import the warehouse data
-import React, { useState, useEffect } from 'react';
-// import camStatus from './components/testing_data/camStatusMock.json';
-// import blockStatus from './components/testing_data/blockStatusMock.json';
-// import headStatus from './components/testing_data/headStatusMock.json';
-// import crankStatus from './components/testing_data/crankStatusMock.json';
 
 function ModelQuantityChart() {
-  const [blockStatus, setBlockStatus] = useState({});
-  const [headStatus, setHeadStatus] = useState({});
-  const [crankStatus, setCrankStatus] = useState({});
-  const [camStatus, setCamStatus] = useState({});
   const [barJson, setBarJson] = useState([]);
-
-  async function fetchBlockStatus() {
-    const response = await fetch('http://localhost:8000/quantity/Block');
-    const data = await response.json();
-    setBlockStatus(data);
-  }
-  async function fetchHeadStatus() {
-    const response = await fetch('http://localhost:8000/quantity/Head');
-    const data = await response.json();
-    setHeadStatus(data);
-  }
-  async function fetchCrankStatus() {
-    const response = await fetch('http://localhost:8000/quantity/Crankshaft');
-    const data = await response.json();
-    setCrankStatus(data);
-  }
-  async function fetchCamStatus() {
-    const response = await fetch('http://localhost:8000/quantity/Camshaft');
-    const data = await response.json();
-    setCamStatus(data);
-  }
-
-  // call the fetchBlockStatus function once
-  useEffect(() => {
-    fetchBlockStatus();
-    fetchHeadStatus();
-    fetchCrankStatus();
-    fetchCamStatus();
-  }, []);
-
   const [isLoading, setIsLoading] = useState(true);
-  // const [chartType, setChartType] = useState('doughnut');
 
-  // function toggleChartType() {
-  //   setChartType(chartType === 'doughnut' ? 'bar' : 'doughnut');
-  //   console.log(chartType);
-  // }
-
-  useEffect(() => {
   async function fetchData() {
     try {
       const types = ['Block', 'Head', 'Crankshaft', 'Camshaft'];
@@ -76,19 +31,46 @@ function ModelQuantityChart() {
     }
   }
 
-  fetchData();
-}, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  function transformBarJson(barJson) {
+    return barJson.map(item => ({
+      type: item.type,
+      model: item.model.map(modelItem => ({
+        model: modelItem.model,
+        qty: modelItem.qty
+      }))
+    }));
+  }
+
+  function parseBarJsonData(barJson) {
+    return barJson.map(item => ({
+      type: item.type,
+      total: item.total
+    }));
+  }
+
+  const donutDashData = parseBarJsonData(barJson);
+  const transformedData = transformBarJson(barJson);
+  console.log('donutDashData:', donutDashData);
 
   return (
     <div>
-      <h2 className='text-4xl font-ligh pb-5'>Overview</h2>
-      <div className='shadow-lg space-x-10 py-10 mb-5 rounded-lg bg-neutral-100 flex flex-row justify-center'>
+      <h2 className='text-4xl font-light pb-5'>Overview</h2>
+      <div className='shadow-lg space-x-20 py-10 mb-5 rounded-lg bg-neutral-100 flex flex-row justify-center'>
         {/* if isLoading */}
         {isLoading && <div>Loading...</div>}
         {/* if not isLoading */}
         {!isLoading && (
-          <div style={{ width: '50%', height: '10%' }}>
-            {barJson.length > 0 && <BarChart data={barJson}/>}
+          <div style={{ display: 'flex', flexDirection: 'row', width: '100%', justifyContent: 'center' }} className='space-x-10'>
+            <div style={{ flex: 1 }}>
+              {transformedData.length > 0 && <BarChart data={transformedData}/>}
+            </div>
+            <div style={{ flex: 1 }}>
+              <DoughnutChart data={donutDashData} chartName={"Type Quantity"}/>
+            </div>
           </div>
         )}
       </div>
@@ -101,7 +83,7 @@ function ModelQuantityChart() {
       </div>
 
       <h2 className='text-2xl font-light pb-5 justify-start'>Warehouse</h2>
-      <div className='shadow-lg flex flex-row space-x-10 py-0 mb-5 bg-neutral-100 rounded-lg bg-neutral-100'>
+      <div className='shadow-lg flex flex-row space-x-10 py-0 mb-5 bg-neutral-100 rounded-lg'>
         <WarehouseDash />
       </div>
 
