3003fcaf85298b90372f7fd5cb410259f7ed5e4d
aingez <aingthawan@gmail.com>
2024-08-28 09:50:54 +0700

    auto format prettier
    for formatting and tailwind

--- src/app/editor/page.jsx
+++ src/app/editor/page.jsx
@@ -1,131 +1,314 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react';
-import { Button, Radio, InputNumber } from 'antd';
-import LayoutMock from '../components/layoutMock';
+import React, { useState } from "react";
+import { Button, Radio, InputNumber } from "antd";
+import LayoutMock from "../components/layoutMock";
 
-function EditorPage() {
-  const [palletMode, setPalletMode] = useState('Create');
-  const [plantType, setPlantType] = useState('Engine');
-  const [plantNumber, setPlantNumber] = useState('1');
-
-  const handlePalletMode = (e) => {
+const PalletEditor = ({
+  palletMode,
+  setPalletMode,
+  plantType,
+  setPlantType,
+  plantNumber,
+  setPlantNumber,
+  handleClear,
+}) => {
+  const handlePalletModeChange = (e) => {
     setPalletMode(e.target.value);
     console.log(`Pallet mode changed to: ${e.target.value}`);
   };
 
-  const handlePlantType = (e) => {
+  const handlePlantTypeChange = (e) => {
     setPlantType(e.target.value);
     console.log(`Plant type changed to: ${e.target.value}`);
   };
 
-  const handlePlantNumber = (e) => {
+  const handlePlantNumberChange = (e) => {
     setPlantNumber(e.target.value);
     console.log(`Plant number changed to: ${e.target.value}`);
   };
 
-  const handleClear = () => {
-    setPalletMode('Create');
-    setPlantType('Engine');
-    setPlantNumber('1');
-  }
-
   return (
-    <div className='pb-20'>
-      <h1 className='text-4xl font-light pb-5'>Data Editor</h1>
-
-      <h2 className='text-2xl font-light pb-5 justify-start'>Pallet Editor</h2>
-      <div className='shadow-lg mb-5 px-5 space-x-5 rounded-lg bg-neutral-100 flex flex-row'>
-        <div className='p-5 space-y-2'>
+    <div className="mb-5 flex flex-row space-x-5 rounded-lg bg-neutral-100 px-5 shadow-lg">
+      <div className="space-y-2 p-5">
+        <div>
+          <label className="mb-1 flex items-center text-xs font-medium">
+            Mode Select
+          </label>
+          <Radio.Group
+            onChange={handlePalletModeChange}
+            size="large"
+            value={palletMode}
+          >
+            <Radio.Button value="Create">CREATE</Radio.Button>
+            <Radio.Button value="Remove">REMOVE</Radio.Button>
+          </Radio.Group>
+        </div>
+        <div className="flex space-x-5">
           <div>
-            <label className="flex items-center mb-1 text-xs font-medium">Mode Select</label>
-            <Radio.Group onChange={handlePalletMode} size="large" value={palletMode}>
-              <Radio.Button value="Create">CREATE</Radio.Button>
-              <Radio.Button value="Remove">REMOVE</Radio.Button>
+            <label className="mb-1 flex items-center text-xs font-medium">
+              Pallet ID
+            </label>
+            <input
+              type="text"
+              className="shadow-xs placeholder-grey-700 block w-full max-w-xs rounded-lg border border-neutral-300 bg-transparent bg-white px-4 py-2 text-sm font-normal leading-relaxed text-gray-200 focus:outline-none"
+              placeholder="XX-XX-X"
+              required
+            />
+          </div>
+          <div>
+            <label className="mb-1 flex items-center text-xs font-medium">
+              Plant Type
+            </label>
+            <Radio.Group
+              onChange={handlePlantTypeChange}
+              size="large"
+              value={plantType}
+              disabled={palletMode === "Remove"}
+            >
+              <Radio.Button value="Engine">Engine</Radio.Button>
+              <Radio.Button value="Casting">Casting</Radio.Button>
             </Radio.Group>
           </div>
-          <div className="flex space-x-5">
-            <div>
-              <label className="flex items-center mb-1 text-xs font-medium">Pallet ID</label>
-              <input type="text" className="block bg-white w-full max-w-xs px-4 py-2 text-sm font-normal shadow-xs text-gray-200 bg-transparent border border-neutral-300 rounded-lg placeholder-grey-700 focus:outline-none leading-relaxed" placeholder="XX-XX-X" required />
-            </div>
-            <div>
-              <label className="flex items-center mb-1 text-xs font-medium">Plant Type</label>
-              <Radio.Group onChange={handlePlantType} size="large" value={plantType} disabled={palletMode === 'Remove'}>
-                <Radio.Button value="Engine">Engine</Radio.Button>
-                <Radio.Button value="Casting">Casting</Radio.Button>
-              </Radio.Group>
-            </div>
-            <div>
-              <label className="flex items-center mb-1 text-xs font-medium">Plant Number</label>
-              <Radio.Group onChange={handlePlantNumber} size="large" value={plantNumber} disabled={palletMode === 'Remove'}>
-                <Radio.Button value="1">1</Radio.Button>
-                <Radio.Button value="2">2</Radio.Button>
-                <Radio.Button value="3">3</Radio.Button>
-              </Radio.Group>
-            </div>
-          </div>
-          <div className='flex flex-row space-x-2 pt-2'>
-            <Button danger type="primary" size="large" style={{ backgroundColor: palletMode === 'Remove' ? '#ef4444' : 'white' }} disabled={palletMode === 'Create'}>
-              REMOVE
-            </Button>
-            <Button type="primary" size="large" style={{ backgroundColor: palletMode === 'Create' ? '#22c55e' : 'white' }} disabled={palletMode === 'Remove'}>
-              CREATE
-            </Button>
-            <Button type="primary" size="large" style={{ backgroundColor: '#f87171' }} onClick={handleClear}>
-              CLEAR
-            </Button>
-            {palletMode == 'Remove' && <p className='text-xs font-light pt-2'>*Able to remove only Unpacked Pallet</p>}
+          <div>
+            <label className="mb-1 flex items-center text-xs font-medium">
+              Plant Number
+            </label>
+            <Radio.Group
+              onChange={handlePlantNumberChange}
+              size="large"
+              value={plantNumber}
+              disabled={palletMode === "Remove"}
+            >
+              <Radio.Button value="1">1</Radio.Button>
+              <Radio.Button value="2">2</Radio.Button>
+              <Radio.Button value="3">3</Radio.Button>
+            </Radio.Group>
           </div>
         </div>
-      </div>
-
-      <h2 className='text-2xl font-light pb-5 justify-start'>Warehouse Editor</h2>
-      <div className='shadow-lg mb-5 px-5 space-x-5 rounded-lg bg-neutral-100 flex flex-row'>
-        <div className='shadow-lg rounded-lg bg-neutral-100 p-10 my-5'>
-          <h3 className='text-1xl font-light pb-5'>Create Layout</h3>
-          <div className="flex space-x-5">
-            <div>
-              <label className="flex items-center mb-1 text-xs font-medium">Plant Type</label>
-              <Radio.Group onChange={handlePlantType} size="large" value={plantType} >
-                <Radio.Button value="Engine">Engine</Radio.Button>
-                <Radio.Button value="Casting">Casting</Radio.Button>
-              </Radio.Group>
-            </div>
-            <div>
-              <label className="flex items-center mb-1 text-xs font-medium">Plant Number</label>
-              <Radio.Group onChange={handlePlantNumber} size="large" value={plantNumber} >
-                <Radio.Button value="1">1</Radio.Button>
-                <Radio.Button value="2">2</Radio.Button>
-                <Radio.Button value="3">3</Radio.Button>
-              </Radio.Group>
-            </div>
-          </div>
-          <div className='pt-5 flex flex-row space-x-5'>
-            <label className="flex items-center mb-1 text-xs font-medium">Lane</label>
-            <InputNumber min={1} max={20} defaultValue={0} />
-            <label className="flex items-center mb-1 text-xs font-medium">Row</label>
-            <InputNumber min={1} max={20} defaultValue={0} />
-          </div>
-          <div className='flex flex-row space-x-2 pt-5'>
-            <Button type="primary" size="large" style={{ backgroundColor: palletMode === 'Create' ? '#22c55e' : 'white' }} disabled={palletMode === 'Remove'}>
-              CREATE
-            </Button>
-            <Button type="primary" size="large" style={{ backgroundColor: '#f87171' }} onClick={handleClear}>
-              CLEAR
-            </Button>
-          </div>
-        </div>
-        <div className='shadow-lg rounded-lg bg-neutral-100 p-10 my-5'>
-          <h3 className='text-1xl font-light pb-5'>Layout Preview</h3>
-          <LayoutMock laneNumber={2} rowNumber={10} />
-        </div>
-        <div className='shadow-lg rounded-lg bg-neutral-100 p-10 my-5'>
-          <h3 className='text-1xl font-light pb-5'>Remove Layout</h3>
+        <div className="flex flex-row space-x-2 pt-2">
+          <Button
+            danger
+            type="primary"
+            size="large"
+            style={{
+              backgroundColor: palletMode === "Remove" ? "#ef4444" : "white",
+            }}
+            disabled={palletMode === "Create"}
+          >
+            REMOVE
+          </Button>
+          <Button
+            type="primary"
+            size="large"
+            style={{
+              backgroundColor: palletMode === "Create" ? "#22c55e" : "white",
+            }}
+            disabled={palletMode === "Remove"}
+          >
+            CREATE
+          </Button>
+          <Button
+            type="primary"
+            size="large"
+            style={{ backgroundColor: "#f87171" }}
+            onClick={handleClear}
+          >
+            CLEAR
+          </Button>
+          {palletMode === "Remove" && (
+            <p className="pt-2 text-xs font-light">
+              *Able to remove only Unpacked Pallet
+            </p>
+          )}
         </div>
       </div>
     </div>
-  )
+  );
+};
+
+const WarehouseEditor = ({
+  palletMode,
+  setPalletMode,
+  plantType,
+  setPlantType,
+  plantNumber,
+  setPlantNumber,
+  handleClear,
+}) => {
+  const [laneNumber, setLaneNumber] = useState(3);
+  const [rowNumber, setRowNumber] = useState(10);
+
+  const handleLaneChange = (value) => {
+    setLaneNumber(value);
+    console.log(`Lane number changed to: ${value}`);
+  };
+
+  const handleRowChange = (value) => {
+    setRowNumber(value);
+    console.log(`Row number changed to: ${value}`);
+  };
+
+  const handlePalletModeChange = (e) => {
+    setPalletMode(e.target.value);
+    console.log(`Pallet mode changed to: ${e.target.value}`);
+  };
+
+  const handlePlantTypeChange = (e) => {
+    setPlantType(e.target.value);
+    console.log(`Plant type changed to: ${e.target.value}`);
+  };
+
+  const handlePlantNumberChange = (e) => {
+    setPlantNumber(e.target.value);
+    console.log(`Plant number changed to: ${e.target.value}`);
+  };
+
+  return (
+    <div className="mb-5 flex flex-row space-x-5 rounded-lg bg-neutral-100 px-5 shadow-lg">
+      <div className="my-5 rounded-lg bg-neutral-100 p-10 shadow-lg">
+        <div className="pb-2">
+          <label className="mb-1 flex items-center text-xs font-medium">
+            Mode Select
+          </label>
+          <Radio.Group
+            onChange={(e) => setPalletMode(e.target.value)}
+            size="large"
+            value={palletMode}
+          >
+            <Radio.Button value="Create">CREATE</Radio.Button>
+            <Radio.Button value="Remove">REMOVE</Radio.Button>
+          </Radio.Group>
+        </div>
+        <div className="flex space-x-5">
+          <div>
+            <label className="mb-1 flex items-center text-xs font-medium">
+              Plant Type
+            </label>
+            <Radio.Group
+              onChange={(e) => setPlantType(e.target.value)}
+              size="large"
+              value={plantType}
+            >
+              <Radio.Button value="Engine">Engine</Radio.Button>
+              <Radio.Button value="Casting">Casting</Radio.Button>
+            </Radio.Group>
+          </div>
+          <div>
+            <label className="mb-1 flex items-center text-xs font-medium">
+              Plant Number
+            </label>
+            <Radio.Group
+              onChange={(e) => setPlantNumber(e.target.value)}
+              size="large"
+              value={plantNumber}
+            >
+              <Radio.Button value="1">1</Radio.Button>
+              <Radio.Button value="2">2</Radio.Button>
+              <Radio.Button value="3">3</Radio.Button>
+            </Radio.Group>
+          </div>
+        </div>
+        <div className="flex flex-row space-x-5 pt-5">
+          <label className="mb-1 flex items-center text-xs font-medium">
+            Lane
+          </label>
+          <InputNumber
+            min={1}
+            max={20}
+            defaultValue={3}
+            onChange={handleLaneChange}
+          />
+          <label className="mb-1 flex items-center text-xs font-medium">
+            Row
+          </label>
+          <InputNumber
+            min={1}
+            max={20}
+            defaultValue={10}
+            onChange={handleRowChange}
+          />
+        </div>
+        <div className="flex flex-row space-x-2 pt-2">
+          <Button
+            danger
+            type="primary"
+            size="large"
+            style={{
+              backgroundColor: palletMode === "Remove" ? "#ef4444" : "white",
+            }}
+            disabled={palletMode === "Create"}
+          >
+            REMOVE
+          </Button>
+          <Button
+            type="primary"
+            size="large"
+            style={{
+              backgroundColor: palletMode === "Create" ? "#22c55e" : "white",
+            }}
+            disabled={palletMode === "Remove"}
+          >
+            CREATE
+          </Button>
+          <Button
+            type="primary"
+            size="large"
+            style={{ backgroundColor: "#f87171" }}
+            onClick={handleClear}
+          >
+            CLEAR
+          </Button>
+        </div>
+      </div>
+      <div className="my-5 rounded-lg bg-neutral-100 p-10 shadow-lg">
+        <h3 className="text-1xl pb-5 font-light">Layout Preview</h3>
+        <LayoutMock laneNumber={laneNumber} rowNumber={rowNumber} />
+      </div>
+    </div>
+  );
+};
+
+function EditorPage() {
+  const [palletMode, setPalletMode] = useState("Create");
+  const [plantType, setPlantType] = useState("Engine");
+  const [plantNumber, setPlantNumber] = useState("1");
+
+  const handleClear = () => {
+    setPalletMode("Create");
+    setPlantType("Engine");
+    setPlantNumber("1");
+  };
+
+  return (
+    <div className="pb-20">
+      <h1 className="pb-5 text-4xl font-light">Data Editor</h1>
+
+      <h2 className="justify-start pb-5 text-2xl font-light">Pallet Editor</h2>
+      <PalletEditor
+        palletMode={palletMode}
+        setPalletMode={setPalletMode}
+        plantType={plantType}
+        setPlantType={setPlantType}
+        plantNumber={plantNumber}
+        setPlantNumber={setPlantNumber}
+        handleClear={handleClear}
+      />
+
+      <h2 className="justify-start pb-5 text-2xl font-light">
+        Warehouse Editor
+      </h2>
+      <WarehouseEditor
+        palletMode={palletMode}
+        setPalletMode={setPalletMode}
+        plantType={plantType}
+        setPlantType={setPlantType}
+        plantNumber={plantNumber}
+        setPlantNumber={setPlantNumber}
+        handleClear={handleClear}
+      />
+    </div>
+  );
 }
 
-export default EditorPage
+export default EditorPage;
