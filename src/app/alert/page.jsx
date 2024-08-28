fca5eb943e0f809acdb944edc6b8b9de4fd51147
aingez <aingthawan@gmail.com>
2024-08-28 11:46:51 +0700

    Update page.jsx

--- src/app/alert/page.jsx
+++ src/app/alert/page.jsx
@@ -5,14 +5,12 @@ import ReusableTable from "../components/alertTable"; // Import the reusable tab
 
 function AlertPage() {
   return (
-    <>
-      <div>
-        <h1 class="custom-title-1">Alert Page</h1>
-      </div>
+    <div className="flex min-h-screen flex-col">
+      <h1 class="custom-title-1">Alert Page</h1>
       <div className="mb-5 rounded-lg bg-neutral-100 px-5 py-5 shadow-lg">
         <ReusableTable pageSize={20} />
       </div>
-    </>
+    </div>
   );
 }
 
