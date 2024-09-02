3e2882b4df3c3d1df23858018b61b68cab5e2d06
aingez <aingthawan@gmail.com>
2024-09-02 09:23:41 +0700

    search page update

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,22 +1,22 @@
 "use client";
 
 import React, { useState } from "react";
-import { Button, DatePicker, Space } from "antd";
 import ReusableTable from "../../components/partSearchTable";
 
 function SearchPage() {
   const [serialInput, setSerialInput] = useState("");
   const [tableKey, setTableKey] = useState(0);
-  const [dateRange, setDateRange] = useState(null);
+  const [dateRange, setDateRange] = useState({ start: "", end: "" });
 
   const handleSerialChange = (e) => {
     setSerialInput(e.target.value);
     console.log("serial " + e.target.value);
   };
 
-  const handleDateChange = (date, dateString) => {
-    setDateRange(dateString);
-    console.log("date " + dateString);
+  const handleDateChange = (e) => {
+    const { name, value } = e.target;
+    setDateRange((prevRange) => ({ ...prevRange, [name]: value }));
+    console.log(`${name} date: ${value}`);
   };
 
   const handleSubmit = (e) => {
@@ -28,14 +28,14 @@ function SearchPage() {
   const handleReset = (e) => {
     e.preventDefault();
     setSerialInput("");
-    setDateRange(null);
+    setDateRange({ start: "", end: "" });
     setTableKey(0);
   };
 
   return (
     <div className="flex min-h-screen flex-col">
       <div className="container mx-auto px-4 py-8">
-        <h1 className="mb-6 text-3xl font-bold">Part Search</h1>
+        <h1 className="custom-title-1">Part Search</h1>
         <div className="custom-box-2">
           <form onSubmit={handleSubmit}>
             <div className="custom-input-layout-1">
@@ -46,43 +46,48 @@ function SearchPage() {
                 onChange={handleSerialChange}
                 className="custom-text-input-1"
                 placeholder="XXXXXXXXXX"
-                disabled={dateRange != null}
+                disabled={dateRange.start || dateRange.end}
               />
             </div>
             <div className="custom-input-layout-1">
               <label>Search Date Range</label>
-              <DatePicker.RangePicker
-                size="large"
-                placeholder={["", "Today"]}
-                allowEmpty={[false, true]}
-                onChange={handleDateChange}
-                disabled={serialInput.length > 0}
-              />
+              <div className="flex space-x-2">
+                <input
+                  type="date"
+                  name="start"
+                  value={dateRange.start}
+                  onChange={handleDateChange}
+                  placeholder="Start Date"
+                  className="block w-full grow rounded-lg border-2 border-neutral-200 bg-transparent bg-white p-2 px-4 py-2 text-sm font-normal leading-relaxed text-gray-400 focus:outline-none disabled:bg-neutral-200"
+                  disabled={serialInput.length > 0}
+                />
+                <span>-</span>
+                <input
+                  type="date"
+                  name="end"
+                  value={dateRange.end}
+                  onChange={handleDateChange}
+                  className="block w-full grow rounded-lg border-2 border-neutral-200 bg-transparent bg-white p-2 px-4 py-2 text-sm font-normal leading-relaxed text-gray-400 focus:outline-none disabled:bg-neutral-200"
+                  disabled={serialInput.length > 0 || dateRange.start === ""}
+                />
+              </div>
             </div>
             <div className="my-2 space-x-2">
-              <Button
+              <button
                 type="submit"
-                size="large"
-                style={{
-                  backgroundColor: "#22c55e",
-                  color: "white",
-                }}
+                className="rounded-lg bg-green-500 px-4 py-2 text-white hover:bg-green-600"
               >
                 SEARCH
-              </Button>
-              <Button
+              </button>
+              <button
                 onClick={handleReset}
                 type="button"
-                size="large"
-                style={{
-                  backgroundColor: "#f87171",
-                  color: "white",
-                }}
+                className="rounded-lg bg-red-500 px-4 py-2 text-white hover:bg-red-600"
               >
                 CLEAR
-              </Button>
+              </button>
             </div>
-            {serialInput.length < 1 && dateRange === null && (
+            {serialInput.length < 1 && !dateRange.start && !dateRange.end && (
               <h2 className="custom-box-title-2">
                 Input the serial number or search by date range to find the part
               </h2>
@@ -90,8 +95,8 @@ function SearchPage() {
           </form>
         </div>
         {tableKey > 0 && (
-          <div className="rounded-lg bg-white p-6 shadow-md">
-            <h3 className="mb-4 text-xl font-semibold">Results</h3>
+          <div className="custom-box-2">
+            <h3 className="custom-box-title-1">Results</h3>
             <ReusableTable key={tableKey} partSerial={serialInput} />
           </div>
         )}
