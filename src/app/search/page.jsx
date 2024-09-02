a15f6b54d4849bc27da59fd554b50af1b1b55e17
aingez <aingthawan@gmail.com>
2024-09-02 08:50:37 +0700

    update date picking field

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,15 +1,22 @@
 "use client";
 
 import React, { useState } from "react";
+import { Button, DatePicker, Space } from "antd";
 import ReusableTable from "../../components/partSearchTable";
 
 function SearchPage() {
   const [serialInput, setSerialInput] = useState("");
   const [tableKey, setTableKey] = useState(0);
+  const [dateRange, setDateRange] = useState(null);
 
-  const handleChange = (e) => {
+  const handleSerialChange = (e) => {
     setSerialInput(e.target.value);
     console.log("serial " + e.target.value);
+  };
+
+  const handleDateChange = (date, dateString) => {
+    setDateRange(dateString);
+    console.log("date " + dateString);
   };
 
   const handleSubmit = (e) => {
@@ -21,54 +28,74 @@ function SearchPage() {
   const handleReset = (e) => {
     e.preventDefault();
     setSerialInput("");
+    setDateRange(null);
     setTableKey(0);
   };
 
   return (
     <div className="flex min-h-screen flex-col">
-      <main className="flex-grow">
-        <div className="container mx-auto px-4 py-8">
-          <h1 className="mb-6 text-3xl font-bold">Part Search</h1>
-          <div className="mb-8 rounded-lg bg-white p-6 shadow-md">
-            <form onSubmit={handleSubmit}>
-              <div className="mb-4">
-                <label className="mb-1 block text-sm font-medium text-gray-700">
-                  Serial Number
-                </label>
-                <input
-                  type="text"
-                  value={serialInput}
-                  onChange={handleChange}
-                  className="block w-full max-w-xs rounded border border-gray-300 px-4 py-2 text-sm shadow-sm focus:border-blue-500 focus:ring-blue-500"
-                  placeholder="XXXXXXXXXX"
-                  required
-                />
-              </div>
-              <div className="flex space-x-4">
-                <button
-                  className="rounded-md bg-red-500 px-4 py-2 text-white hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
-                  onClick={handleReset}
-                  type="button"
-                >
-                  Reset
-                </button>
-                <button
-                  className="rounded-md bg-green-500 px-4 py-2 text-white hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
-                  type="submit"
-                >
-                  Search
-                </button>
-              </div>
-            </form>
-          </div>
-          {tableKey > 0 && (
-            <div className="rounded-lg bg-white p-6 shadow-md">
-              <h3 className="mb-4 text-xl font-semibold">Results</h3>
-              <ReusableTable key={tableKey} partSerial={serialInput} />
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="mb-6 text-3xl font-bold">Part Search</h1>
+        <div className="custom-box-2">
+          <form onSubmit={handleSubmit}>
+            <div className="custom-input-layout-1">
+              <label>Serial Number</label>
+              <input
+                type="text"
+                value={serialInput}
+                onChange={handleSerialChange}
+                className="custom-text-input-1"
+                placeholder="XXXXXXXXXX"
+                disabled={dateRange != null}
+              />
             </div>
-          )}
+            <div className="custom-input-layout-1">
+              <label>Search Date Range</label>
+              <DatePicker.RangePicker
+                size="large"
+                placeholder={["", "Today"]}
+                allowEmpty={[false, true]}
+                onChange={handleDateChange}
+                disabled={serialInput.length > 0}
+              />
+            </div>
+            <div className="my-2 space-x-2">
+              <Button
+                type="submit"
+                size="large"
+                style={{
+                  backgroundColor: "#22c55e",
+                  color: "white",
+                }}
+              >
+                SEARCH
+              </Button>
+              <Button
+                onClick={handleReset}
+                type="button"
+                size="large"
+                style={{
+                  backgroundColor: "#f87171",
+                  color: "white",
+                }}
+              >
+                CLEAR
+              </Button>
+            </div>
+            {serialInput.length < 1 && dateRange === null && (
+              <h2 className="custom-box-title-2">
+                Input the serial number or search by date range to find the part
+              </h2>
+            )}
+          </form>
         </div>
-      </main>
+        {tableKey > 0 && (
+          <div className="rounded-lg bg-white p-6 shadow-md">
+            <h3 className="mb-4 text-xl font-semibold">Results</h3>
+            <ReusableTable key={tableKey} partSerial={serialInput} />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
