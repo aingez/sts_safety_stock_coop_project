7b58b1b378b08ecbfb04b2306b68d220d976eff3
aingez <aingthawan@gmail.com>
2024-09-11 13:36:35 +0700

    edit color scheme

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,40 +10,40 @@ function Nav() {
   return (
     <ul className="justify-left mb-10 flex bg-black p-3 text-[25px]">
       <li
-        className={`mx-2 text-white transition-all hover:text-yellow-200 ${pathname === "/" ? "font-bold text-yellow-500 dark:text-fuchsia-500" : ""}`}
+        className={`mx-2 text-white transition-all hover:text-yellow-200 ${pathname === "/" ? "font-bold text-yellow-500" : ""}`}
       >
         <Link href="/">M-Stock</Link>
       </li>
       <div className="mx-2 border-r border-white" />
       <li
-        className={`mx-2 text-white transition-all hover:text-yellow-200 ${pathname === "/pack" ? "font-bold text-yellow-500 dark:text-fuchsia-500" : ""}`}
+        className={`mx-2 text-white transition-all hover:text-yellow-200 ${pathname === "/pack" ? "font-bold text-yellow-500" : ""}`}
       >
         <Link href="/pack">Pack</Link>
       </li>
       <li
-        className={`mx-2 text-white transition-all hover:text-yellow-200 ${pathname === "/unpack" ? "font-bold text-yellow-500 dark:text-fuchsia-500" : ""}`}
+        className={`mx-2 text-white transition-all hover:text-yellow-200 ${pathname === "/unpack" ? "font-bold text-yellow-500" : ""}`}
       >
         <Link href="/unpack">Unpack</Link>
       </li>
       <div className="mx-2 border-r border-white" />
       <li
-        className={`mx-2 text-white transition-all hover:text-yellow-200 ${pathname === "/alert" ? "font-bold text-yellow-500 dark:text-fuchsia-500" : ""}`}
+        className={`mx-2 text-white transition-all hover:text-yellow-200 ${pathname === "/alert" ? "font-bold text-yellow-500" : ""}`}
       >
         <Link href="/alert">Alert</Link>
       </li>
       <li
-        className={`mx-2 text-white transition-all hover:text-yellow-200 ${pathname === "/search" ? "font-bold text-yellow-500 dark:text-fuchsia-500" : ""}`}
+        className={`mx-2 text-white transition-all hover:text-yellow-200 ${pathname === "/search" ? "font-bold text-yellow-500" : ""}`}
       >
         <Link href="/search">Search</Link>
       </li>
       <div className="mx-2 border-r border-white" />
       <li
-        className={`mx-2 text-white transition-all hover:text-yellow-200 ${pathname === "/manage" ? "font-bold text-yellow-500 dark:text-fuchsia-500" : ""}`}
+        className={`mx-2 text-white transition-all hover:text-yellow-200 ${pathname === "/manage" ? "font-bold text-yellow-500" : ""}`}
       >
         <Link href="/manage">Manage</Link>
       </li>
       <li
-        className={`mx-2 text-white transition-all hover:text-yellow-200 ${pathname === "/editor" ? "font-bold text-yellow-500 dark:text-fuchsia-500" : ""}`}
+        className={`mx-2 text-white transition-all hover:text-yellow-200 ${pathname === "/editor" ? "font-bold text-yellow-500" : ""}`}
       >
         <Link href="/editor">Edit</Link>
       </li>
--- src/components/qtyDisp.jsx
+++ src/components/qtyDisp.jsx
@@ -25,32 +25,22 @@ const QuantityDisplay = () => {
   const { summary, components } = data.data;
 
   return (
-    <div className="space-y-4 p-4 md:p-6 lg:p-8">
+    <div className="space-y-4 p-4">
       {Object.keys(summary).map((key) => (
         <div key={key} className="space-y-2">
-          <div className="grid grid-cols-2 gap-1 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-5 xl:grid-cols-5">
-            <button
-              // className="relative inline-block overflow-hidden rounded-md py-1 text-center text-sm font-bold uppercase text-green-400 shadow-xl shadow-sky-800 transition-all sm:px-3 sm:py-2 sm:text-base md:px-4 md:py-3 md:text-lg"
+          <div className="grid grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-5 xl:grid-cols-5">
+            <div
+              className="custom-display-box-1"
               style={{
-                background: "linear-gradient(to right, #6b21a8, #1d4ed8)",
-                transform: "skew(-20deg)",
+                background: "linear-gradient(to right, #f59e0b, #fbbf24)",
               }}
-              className="custom-button-1-red"
             >
               {key} : {summary[key]}
-            </button>
+            </div>
             {components[key].map((model) => (
-              <button
-                key={model.model}
-                // className="relative inline-block overflow-hidden rounded-md py-1 text-center text-sm font-bold uppercase text-green-700 shadow-xl shadow-amber-800 transition-all sm:px-3 sm:py-2 sm:text-base md:px-4 md:py-3 md:text-lg"
-                style={{
-                  background: "linear-gradient(to right, #ca8a04, #eab308)",
-                  transform: "skew(-20deg)",
-                }}
-                className="custom-button-1-green"
-              >
+              <div key={model.model} className="custom-display-box-2">
                 {model.model} : {model.qty}
-              </button>
+              </div>
             ))}
           </div>
         </div>
