af376338e64a7a73660fcdab17a0ffefc41f0ccb
aingez <aingthawan@gmail.com>
2024-09-12 15:33:14 +0700

    add pallet creator/remover

--- src/app/editor/page.jsx
+++ src/app/editor/page.jsx
@@ -3,74 +3,23 @@
 import React, { useState } from "react";
 // import LayoutMock from "../../components/layoutMock";
 import WhCreator from "../../components/warehouseCreator";
+import PalletCreateRemover from "../../components/palletCreateRem";
 
-const WarehouseEditor = ({
-  palletMode,
-  setPalletMode,
-  plantType,
-  setPlantType,
-  plantNumber,
-  setPlantNumber,
-  handleClear,
-}) => {
-  const [laneNumber, setLaneNumber] = useState(3);
-  const [rowNumber, setRowNumber] = useState(10);
-
-  const handleLaneChange = (value) => {
-    setLaneNumber(value);
-    console.log(`Lane number changed to: ${value}`);
-  };
-
-  const handleRowChange = (value) => {
-    setRowNumber(value);
-    console.log(`Row number changed to: ${value}`);
-  };
-
-  const handlePalletModeChange = (e) => {
-    setPalletMode(e.target.value);
-    console.log(`Pallet mode changed to: ${e.target.value}`);
-  };
-
-  const handlePlantTypeChange = (e) => {
-    setPlantType(e.target.value);
-    console.log(`Plant type changed to: ${e.target.value}`);
-  };
-
-  const handlePlantNumberChange = (e) => {
-    setPlantNumber(e.target.value);
-    console.log(`Plant number changed to: ${e.target.value}`);
-  };
-
-  return (
-    <div className="custom-box-1">
-      <div className="custom-box-2">
-        <WhCreator />
-      </div>
-    </div>
-  );
-};
-
-function EditorPage() {
-  const [palletMode, setPalletMode] = useState("Create");
-  const [plantType, setPlantType] = useState("Engine");
-  const [plantNumber, setPlantNumber] = useState("1");
-
-  const handleClear = () => {
-    setPalletMode("Create");
-    setPlantType("Engine");
-    setPlantNumber("1");
-  };
-
+const WarehouseEditor = () => {
   return (
     <div className="min-h-screen pb-20">
       <h1 className="custom-title-1">Data Editor</h1>
       <h2 className="custom-subtitle-2">Pallet Editor</h2>
+      <div className="custom-box-1 py-4">
+        <PalletCreateRemover />
+      </div>
+
       <h2 className="custom-subtitle-2">Warehouse Editor</h2>
       <div className="custom-box-1 overflow-x-auto">
         <WhCreator />
       </div>
     </div>
   );
-}
+};
 
-export default EditorPage;
+export default WarehouseEditor;
