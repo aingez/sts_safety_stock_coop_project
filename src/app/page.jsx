0558a810dde84c6c07b05a6e0eaf1f52bba26a10
aingez <aingthawan@gmail.com>
2024-09-26 14:54:58 +0700

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -69,7 +69,7 @@ function HomePage() {
             <QuantityDisplay />
           </div>
         </div>
-        <div className="custom-box-3 flex flex-col overflow-scroll">
+        <div className="custom-box-3 flex w-full flex-col overflow-scroll">
           <h2 className="custom-title-2">
             Reminder :
             <p className="custom-box-title-2">
