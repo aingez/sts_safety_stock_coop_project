f5e40de633ba2ce6fb4cde9cc5e2463edfbfa63a
aingez <aingthawan@gmail.com>
2024-09-26 15:17:12 +0700

    Update page.jsx

--- src/app/creator/page.jsx
+++ src/app/creator/page.jsx
@@ -13,18 +13,18 @@ const WarehouseEditor = () => {
     <div className="min-h-screen pb-20">
       <h1 className="custom-title-1">Create Master</h1>
       <h2 className="custom-subtitle-2">Pallet Creator</h2>
-      <div className="custom-box-1 py-4">
+      <div className="custom-box-3 py-4">
         <PalletCreateRemover />
       </div>
 
       <h2 className="custom-subtitle-2">Warehouse Creator</h2>
-      <div className="custom-box-1 overflow-x-auto">
+      <div className="custom-box-3 overflow-x-auto">
         <WhCreator />
       </div>
 
       <h2 className="custom-subtitle-2">Parts Creator</h2>
-      <div className="custom-box-1 overflow-x-auto">
-        <div className="custom-box-2 m-4">
+      <div className="custom-box-3 overflow-x-auto">
+        <div className="custom-box-2 m-4 w-full">
           <PartList />
         </div>
       </div>
