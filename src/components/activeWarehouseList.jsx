4cb2069f5e938e13600162363f61bf2221ea243e
aingez <aingthawan@gmail.com>
2024-09-28 09:34:20 +0700

    Update activeWarehouseList.jsx

--- src/components/activeWarehouseList.jsx
+++ src/components/activeWarehouseList.jsx
@@ -30,33 +30,37 @@ const ActiveWarehouseList = () => {
         <div>
           <h1 className="custom-box-title-1">Active Warehouse</h1>
           <div className="max-h-96 overflow-auto">
-            <table className="min-w-full text-left text-gray-500 rtl:text-right dark:text-gray-400">
-              <thead className="bg-gray-100 uppercase text-gray-700 dark:bg-neutral-500 dark:text-neutral-200">
-                <tr>
-                  <th scope="col" className="px-4 py-3 sm:px-6">
-                    Key
-                  </th>
-                  <th scope="col" className="px-4 py-3 sm:px-6">
-                    Type
-                  </th>
-                  <th scope="col" className="px-4 py-3 sm:px-6">
-                    ID
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {data.data.map((warehouse) => (
-                  <tr
-                    key={warehouse.plant_key}
-                    className="border-b bg-white hover:bg-gray-100 dark:border-neutral-500 dark:bg-neutral-700 dark:hover:bg-gray-600 dark:hover:text-white"
-                  >
-                    <td className="px-4 py-4">{warehouse.plant_key}</td>
-                    <td className="px-4 py-4">{warehouse.plant_type}</td>
-                    <td className="px-4 py-4">{warehouse.plant_id}</td>
+            {data.data.length > 0 ? (
+              <table className="min-w-full text-left text-gray-500 rtl:text-right dark:text-gray-400">
+                <thead className="bg-gray-100 uppercase text-gray-700 dark:bg-neutral-500 dark:text-neutral-200">
+                  <tr>
+                    <th scope="col" className="px-4 py-3 sm:px-6">
+                      Key
+                    </th>
+                    <th scope="col" className="px-4 py-3 sm:px-6">
+                      Type
+                    </th>
+                    <th scope="col" className="px-4 py-3 sm:px-6">
+                      ID
+                    </th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {data.data.map((warehouse) => (
+                    <tr
+                      key={warehouse.plant_key}
+                      className="border-b bg-white hover:bg-gray-100 dark:border-neutral-500 dark:bg-neutral-700 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      <td className="px-4 py-4">{warehouse.plant_key}</td>
+                      <td className="px-4 py-4">{warehouse.plant_type}</td>
+                      <td className="px-4 py-4">{warehouse.plant_id}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <div>No active warehouses available.</div>
+            )}
           </div>
         </div>
       ) : (
